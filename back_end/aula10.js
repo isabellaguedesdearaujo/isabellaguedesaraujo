44fc0144f0ef4ac2bafddc2802eeb418086c4fcc
isabellaguedesdearaujo <isabellaguedesaraujo4@gmail.com>
2026-02-26 13:53:29 -0300

    Delete back_end/aula10.js

--- back_end/aula10.js
+++
@@ -1,6 +0,0 @@
-const usuarios = [{id: 1, nome: 'Alan Turing'}, {id: 2, nome: 'Grace Hopper'}];
-
-// Executa uma função de callback para cada elemento do array
-usuarios.forEach((usuario, index) => {
-    console.log(`Usuário ${index + 1}: ${usuario.nome}`);
-});
