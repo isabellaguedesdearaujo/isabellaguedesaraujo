27f26c0474f899e7ce87c6dffdf27ca57e8b08e2
isabellaguedesdearaujo <isabellaguedesaraujo4@gmail.com>
2026-04-14 08:07:20 -0300

    Create meuPrimeiroProjeto.js with project configuration
    Initial project setup with package details and scripts.

---
+++ back_end/meuPrimeiroProjeto.js
@@ -0,0 +1,16 @@
+{
+    "name": "meu-projeto",
+    "version": "1.0.0",
+    "descriptiom": "meu primeiro projeto",
+    "main": "index.js",
+    "scripts": {
+        "start": "node index.js",
+        "build": "webpack --mode production"
+    },
+    "dependencies": {
+        "react": "^18.2.0"
+    },
+    "devDependecies": {
+        "eslint": "^8.0.0"
+    }
+}
