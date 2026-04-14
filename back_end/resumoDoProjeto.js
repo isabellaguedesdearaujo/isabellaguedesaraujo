99e6ce32aaada37b7dd01ad6713aeca4f22f71b6
isabellaguedesdearaujo <isabellaguedesaraujo4@gmail.com>
2026-04-14 08:06:43 -0300

    Create resumoDoProjeto.js

---
+++ back_end/resumoDoProjeto.js
@@ -0,0 +1,6 @@
+console.log('Cabeçalho do projeto vem aqui');
+console.log('Nome da escola: Escola do Futuro José Luiz bittencourt do Estado de Goiás ');
+console.log('Nome do curso: desenvolvimento web e cybersegurança');
+console.log('Nome da diciplina: BackEnd');
+console.log('Nome do Professor: George Mendes Marra');
+console.log('Título do Projeto: Leitura e Escrita'); de Arquivos com Node.js
