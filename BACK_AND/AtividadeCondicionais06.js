0656707b660125303b709e4877386e876625d637
isabellaguedesdearaujo <isabellaguedesaraujo4@gmail.com>
2026-08-25 14:54:07 -0300

    Update AtividadeCondicionais06.js

--- BACK_AND/AtividadeCondicionais06.js
+++ BACK_AND/AtividadeCondicionais06.js
@@ -5,19 +5,19 @@ const rl = readline.createInterface({
     output : process.stdout,
 });
 
-rl.question("Dígite seu nome",(nome)=>{
-rl.question("Dígite valor do saldo",(saldo)=>{
-rl.question("Dígite valor do saque",(saque)=>{
+rl.question("Dígite seu nome: ",(nome)=>{
+rl.question("Dígite valor do saldo: ",(saldo)=>{
+rl.question("Dígite valor do saque: ",(saque)=>{
 
     saldo = Number (saldo)
     saque = Number (saque)
     
 
 
-    if(saldo > saque ){
+    if(saque > saldo ){
         console.log("Saldo insuficiente");
-    }else if (saldo  saque){
-        valor = saldo - saque
+    }else if (saldo > saque){
+        const valor = saldo - saque
         console.log("Saque realizado agora seu saldo disponivel é de: " + valor );
     };
         
